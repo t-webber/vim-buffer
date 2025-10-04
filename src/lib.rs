932c7baabc71c39346dda0dc1d66be3bd30968dc
Tom Webber <t.c.w.webber@gmail.com>
2025-10-04 07:25:47 +0200

    chore(lint): add clippy rules

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+#![doc = include_str!("../README.md")]
+#![warn(
+    missing_docs,
+    warnings,
+    deprecated_safe,
+    future_incompatible,
+    keyword_idents,
+    let_underscore,
+    nonstandard_style,
+    refining_impl_trait,
+    rust_2018_compatibility,
+    rust_2018_idioms,
+    rust_2021_compatibility,
+    rust_2024_compatibility,
+    unused,
+    clippy::all,
+    clippy::pedantic,
+    clippy::style,
+    clippy::perf,
+    clippy::complexity,
+    clippy::correctness,
+    clippy::restriction,
+    clippy::nursery,
+    // clippy::cargo
+)]
+#![expect(clippy::doc_include_without_cfg, reason = "see issue #13918")]
+#![expect(clippy::blanket_clippy_restriction_lints, reason = "I want them all")]
