90f194ab3ecf6d7d19e08e39a5101b1b41a27a3c
Tom Webber <t.c.w.webber@gmail.com>
2025-10-04 08:04:14 +0200

    feat(mode): implement HandleEvent for Normal mode

--- src/mode/mod.rs
+++ src/mode/mod.rs
@@ -6,6 +6,8 @@ mod normal;
 use crossterm::event::Event;
 
 use crate::action::Action;
+use crate::mode::insert::Insert;
+use crate::mode::normal::Normal;
 
 /// Represents the vim mode of the buffer.
 #[non_exhaustive]
@@ -28,4 +30,13 @@ pub enum Mode {
 trait HandleEvent {
     /// Handle incomming terminal events, like keypresses.
     fn handle_event(self, event: Event) -> Option<Action>;
+}
+
+impl HandleEvent for Mode {
+    fn handle_event(self, event: Event) -> Option<Action> {
+        match self {
+            Self::Insert => Insert.handle_event(event),
+            Self::Normal => Normal.handle_event(event),
+        }
+    }
 }
--- src/mode/normal.rs
+++ src/mode/normal.rs
@@ -1,2 +1,22 @@
-/// Struct to handle keypresses in normal mode
+use crossterm::event::{Event, KeyCode};
+
+use crate::Mode;
+use crate::action::Action;
+use crate::mode::HandleEvent;
+
+/// Struct to handle keypresses in insert mode
 pub struct Normal;
+
+#[expect(
+    clippy::wildcard_enum_match_arm,
+    reason = "partially implement events"
+)]
+impl HandleEvent for Normal {
+    fn handle_event(self, event: Event) -> Option<Action> {
+        let key_press_event = event.as_key_press_event()?;
+        match key_press_event.code {
+            KeyCode::Char('i') => Some(Action::SelectMode(Mode::Insert)),
+            _ => None,
+        }
+    }
+}
