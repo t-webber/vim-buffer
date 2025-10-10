dbc486283da5e15b4b355502839525069cde122d
Tom Webber <t.c.w.webber@gmail.com>
2025-10-10 19:21:58 +0200

    ref(tests): better unit testing

--- src/tests.rs
+++ src/tests.rs
@@ -1,36 +1,52 @@
 use crossterm::event::{Event, KeyCode, KeyEvent};
 
-use crate::Buffer;
-use crate::mode::Mode;
+use crate::{Buffer, Mode};
 
-fn char_event(ch: char) -> Event {
-    Event::Key(KeyEvent::from(KeyCode::Char(ch)))
+macro_rules! evt {
+    ($buffer:ident, $name:ident) => {
+        evt!($buffer, KeyCode::$name)
+    };
+    ($buffer:ident, $name:literal) => {
+        evt!($buffer, KeyCode::Char($name))
+    };
+    ($buffer:ident, $name:expr) => {
+        $buffer.update(&Event::Key(KeyEvent::from($name)))
+    };
 }
 
 #[test]
 fn do_nothing() {
     let mut buffer = Buffer::default();
-    assert!(!buffer.update(&Event::Key(KeyEvent::from(KeyCode::Enter))));
+    assert!(!evt!(buffer, Enter));
+    assert!(evt!(buffer, 'i'));
+    assert!(!evt!(buffer, Enter));
+}
+
+#[test]
+fn backspace() {
+    let mut buffer = Buffer::default();
+    assert!(evt!(buffer, 'i'));
+    assert!(evt!(buffer, 'a'));
+    assert!(evt!(buffer, Backspace));
+    assert!(buffer.as_content().is_empty());
+}
+
+#[test]
+fn mode_switch() {
+    let mut buffer = Buffer::default();
+    assert_eq!(buffer.as_mode(), Mode::Normal);
+    assert!(evt!(buffer, 'i'));
+    assert_eq!(buffer.as_mode(), Mode::Insert);
+    assert!(evt!(buffer, Esc));
+    assert_eq!(buffer.as_mode(), Mode::Normal);
 }
 
 #[test]
 fn hello_world() {
     let mut buffer = Buffer::default();
-    assert!(buffer.update(&char_event('i')));
-    for ch in "Hello".chars() {
-        assert!(buffer.update(&char_event(ch)));
-    }
-    assert!(matches!(buffer.as_mode(), Mode::Insert));
-    assert!(buffer.update(&Event::Key(KeyEvent::from(KeyCode::Esc))));
-    assert!(matches!(buffer.as_mode(), Mode::Normal));
-    for ch in "someotherchars".chars() {
-        assert!(!buffer.update(&char_event(ch)));
-    }
-    assert!(matches!(buffer.as_mode(), Mode::Normal));
-    assert!(buffer.update(&char_event('i')));
-    assert!(matches!(buffer.as_mode(), Mode::Insert));
-    for ch in " World".chars() {
-        assert!(buffer.update(&char_event(ch)));
+    assert!(evt!(buffer, 'i'));
+    for ch in "Hello World".chars() {
+        assert!(evt!(buffer, KeyCode::Char(ch)));
     }
     assert!(buffer.as_content() == "Hello World");
 }
