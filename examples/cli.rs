b419433a8e5df1b680ef4b0dc2292a8fb38f5640
Tom Webber <t.c.w.webber@gmail.com>
2025-11-22 10:23:54 +0100

    ref(example): make the cli example more readable

--- examples/cli.rs
+++ examples/cli.rs
@@ -1,56 +1,70 @@
+use core::fmt;
 use core::iter::repeat_n;
 use std::io::{Write, stdout};
 
 use color_eyre as colour_eyre; // ignore-spell
-use crossterm::event::{self, KeyModifiers};
+use crossterm::event::{self, Event, KeyModifiers};
 use crossterm::terminal::disable_raw_mode;
 use vim_buffer::crossterm::terminal::enable_raw_mode;
 use vim_buffer::{Buffer, Mode};
 
-const RESET: &str = "\x1b[0m";
-const NORMAL: ModeDisplay =
-    ModeDisplay { colour: "\x1b[32m", prompt: "normal >>> " };
-const INSERT: ModeDisplay =
-    ModeDisplay { colour: "\x1b[36m", prompt: "insert >>> " };
-const _: () = assert!(NORMAL.prompt.len() == INSERT.prompt.len());
-const DISPLAY_MODE_LEN: usize = NORMAL.prompt.len();
+pub const RESET_COLOUR: &str = "\x1b[0m";
+pub const RED: &str = "\x1b[31m";
+pub const GREEN: &str = "\x1b[32m";
+pub const CYAN: &str = "\x1b[36m";
 
-struct ModeDisplay {
+const NORMAL: ModePrompt = ModePrompt { colour: CYAN, prompt: "normal >>> " };
+const INSERT: ModePrompt = ModePrompt { colour: GREEN, prompt: "insert >>> " };
+const UNKNOWN: ModePrompt = ModePrompt { colour: RED, prompt: "unkown >>> " };
+
+const _: () = assert!(NORMAL.prompt.len() == INSERT.prompt.len());
+const _: () = assert!(UNKNOWN.prompt.len() == INSERT.prompt.len());
+const MODE_PROMPT_LEN: usize = NORMAL.prompt.len();
+
+/// Prompt displayed for a given vim mode.
+struct ModePrompt {
     colour: &'static str,
     prompt: &'static str,
 }
 
-impl ModeDisplay {
-    fn display(&self) -> String {
+impl fmt::Display for ModePrompt {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let Self { colour, prompt } = self;
-        format!("{colour}{prompt}{RESET}")
+        write!(f, "{colour}{prompt}{}", RESET_COLOUR)
     }
 }
 
-impl From<Mode> for ModeDisplay {
-    fn from(value: Mode) -> Self {
-        match value {
-            Mode::Insert => INSERT,
-            Mode::Normal => NORMAL,
-            _ => panic!("unsupported"),
-        }
-    }
+/// Prints some spaces to clear the previous buffer, whose content was of length
+/// `previous_len`.
+fn clear_line(previous_len: usize) {
+    let nb_spaces = previous_len + MODE_PROMPT_LEN;
+    let spaces = repeat_n(' ', nb_spaces).collect::<String>();
+    print!("\r{spaces}\r")
 }
 
-fn print_current(
-    buffer: &Buffer,
-    previous_len: usize,
-) -> colour_eyre::Result<()> {
-    let spaces =
-        repeat_n(' ', previous_len + DISPLAY_MODE_LEN).collect::<Box<str>>();
-    let cursor = buffer.as_cursor() + DISPLAY_MODE_LEN;
-    print!(
-        "\r{spaces}\r{}{}\r\x1b[{cursor}C",
-        ModeDisplay::from(buffer.as_mode()).display(),
-        buffer.as_content()
-    );
-    stdout().flush()?;
-    Ok(())
+/// Prints the [`Buffer`] with the mode it is in, and it's contents.
+fn print_buffer(buffer: &Buffer) {
+    let cursor = buffer.as_cursor() + MODE_PROMPT_LEN;
+    let mode = match buffer.as_mode() {
+        Mode::Insert => INSERT,
+        Mode::Normal => NORMAL,
+        _ => panic!("unsupported"),
+    };
+    let content = buffer.as_content();
+    print!("{mode}{content}\r\x1b[{cursor}C",);
+}
+
+/// Checks whether the given event is `<C-c>` or not.
+fn is_ctrl_c(event: &Event) -> bool {
+    if let Some(key_event) = event.as_key_press_event()
+        && key_event.modifiers == KeyModifiers::CONTROL
+        && let Some(ch) = key_event.code.as_char()
+        && ch == 'c'
+    {
+        true
+    } else {
+        false
+    }
 }
 
 fn main() -> colour_eyre::Result<()> {
@@ -58,25 +72,24 @@ fn main() -> colour_eyre::Result<()> {
     println!("Press <C-c> to exit");
     enable_raw_mode()?;
 
-    let mut buffer = Buffer::default();
     let mut previous_len = 0;
+    let mut buffer = Buffer::default();
 
     loop {
-        print_current(&buffer, previous_len)?;
-        previous_len = buffer.as_content().len();
+        clear_line(previous_len);
+        print_buffer(&buffer);
+        stdout().flush()?;
 
         let event = event::read()?;
-        if let Some(key_event) = event.as_key_press_event()
-            && key_event.modifiers & KeyModifiers::CONTROL
-                == KeyModifiers::CONTROL
-            && let Some(ch) = key_event.code.as_char()
-            && ch == 'c'
-        {
-            disable_raw_mode()?;
-            println!();
-            break Ok(());
+        if is_ctrl_c(&event) {
+            break;
         }
 
+        previous_len = buffer.as_content().len();
         buffer.update(&event);
     }
+
+    disable_raw_mode()?;
+    println!();
+    Ok(())
 }
